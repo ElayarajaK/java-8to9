316ca712fd35ada04d086f2dc2bc277e883d27e9
Elayaraja.K <kelaya5@gmail.com>
2023-01-31 07:42:07 +0530

    commit

---
+++ rev/Java17.java
@@ -0,0 +1,17 @@
+class Java17{
+
+		public static void main(String[]name)
+		{
+			int number=10;
+			System.out.println( "number : "+number);
+			System.out.println("number : "+number);
+			System.out.println("number : "+number);
+			System.out.println(number+number);
+			System.out.println(20*2);
+			System.out.println(20/2);
+			System.out.println(20%2);
+			System.out.println(20-2);			
+			System.out.println(number + number +"Hello");			
+			System.out.println(number + number +"Hello"+(number-number));
+			}
+}
